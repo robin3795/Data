c6f225a5b346891672d35d2b66a264efd8b1e989
Robin <robinli@live.ca>
2017-07-03 22:37:30 -0400

    update

--- ExcelLibrary.py
+++ ExcelLibrary.py
@@ -21,15 +21,10 @@ class ExcelLibrary:
     """
     This test library provides keywords to allow opening, reading, writing
      and saving Excel files from Robot Framework.
-
-
     *Before running tests*
-
     Prior to running tests, ExcelLibrary must first be imported into your Robot test suite.
-
     Example:
         | Library | ExcelLibrary |
-
     """
 
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
@@ -50,15 +45,12 @@ class ExcelLibrary:
         """
         Opens the Excel file from the path provided in the file name parameter.
         If the boolean useTempDir is set to true, depending on the operating system of the computer running the test the file will be opened in the Temp directory if the operating system is Windows or tmp directory if it is not.
-
         Arguments:
                 |  File Name (string)                      | The file name string value that will be used to open the excel file to perform tests upon.                                  |
                 |  Use Temporary Directory (default=False) | The file will not open in a temporary directory by default. To activate and open the file in a temporary directory, pass 'True' in the variable. |
         Example:
-
         | *Keywords*           |  *Parameters*                                      |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
-
         """
         if useTempDir is True:
             print 'Opening file at %s' % filename
@@ -71,14 +63,11 @@ class ExcelLibrary:
     def open_excel_current_directory(self, filename):
         """
         Opens the Excel file from the current directory using the directory the test has been run from.
-
         Arguments:
                 |  File Name (string)  | The file name string value that will be used to open the excel file to perform tests upon.  |
         Example:
-
         | *Keywords*           |  *Parameters*        |
         | Open Excel           |  ExcelRobotTest.xls  |
-
         """
         workdir = os.getcwd()
         print 'Opening file at %s' % filename
@@ -88,13 +77,10 @@ class ExcelLibrary:
     def get_sheet_names(self):
         """
         Returns the names of all the worksheets in the current workbook.
-
         Example:
-
         | *Keywords*              |  *Parameters*                                      |
         | Open Excel              |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Sheets Names        |                                                    |
-
         """
         sheetNames = self.wb.sheet_names()
         return sheetNames
@@ -102,13 +88,10 @@ class ExcelLibrary:
     def get_number_of_sheets(self):
         """
         Returns the number of worksheets in the current workbook.
-
         Example:
-
         | *Keywords*              |  *Parameters*                                      |
         | Open Excel              |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Number of Sheets    |                                                    |
-
         """
         sheetNum = self.wb.nsheets
         return sheetNum
@@ -116,15 +99,12 @@ class ExcelLibrary:
     def get_column_count(self, sheetname):
         """
         Returns the specific number of columns of the sheet name specified.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the column count will be returned from. |
         Example:
-
         | *Keywords*          |  *Parameters*                                      |
         | Open Excel          |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Column Count    |  TestSheet1                                        |
-
         """
         sheet = self.wb.sheet_by_name(sheetname)
         return sheet.ncols
@@ -132,15 +112,12 @@ class ExcelLibrary:
     def get_row_count(self, sheetname):
         """
         Returns the specific number of rows of the sheet name specified.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the row count will be returned from. |
         Example:
-
         | *Keywords*          |  *Parameters*                                      |
         | Open Excel          |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Row Count       |  TestSheet1                                        |
-
         """
         sheet = self.wb.sheet_by_name(sheetname)
         return sheet.nrows
@@ -148,17 +125,14 @@ class ExcelLibrary:
     def get_column_values(self, sheetname, column, includeEmptyCells=True):
         """
         Returns the specific column values of the sheet name specified.
-
         Arguments:
                 |  Sheet Name (string)                 | The selected sheet that the column values will be returned from.                                                            |
                 |  Column (int)                        | The column integer value that will be used to select the column from which the values will be returned.                     |
                 |  Include Empty Cells (default=True)  | The empty cells will be included by default. To deactivate and only return cells with values, pass 'False' in the variable. |
         Example:
-
         | *Keywords*           |  *Parameters*                                          |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |   |
         | Get Column Values    |  TestSheet1                                        | 0 |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -178,17 +152,14 @@ class ExcelLibrary:
     def get_row_values(self, sheetname, row, includeEmptyCells=True):
         """
         Returns the specific row values of the sheet name specified.
-
         Arguments:
                 |  Sheet Name (string)                 | The selected sheet that the row values will be returned from.                                                               |
                 |  Row (int)                           | The row integer value that will be used to select the row from which the values will be returned.                           |
                 |  Include Empty Cells (default=True)  | The empty cells will be included by default. To deactivate and only return cells with values, pass 'False' in the variable. |
         Example:
-
         | *Keywords*           |  *Parameters*                                          |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |   |
         | Get Row Values       |  TestSheet1                                        | 0 |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -208,16 +179,13 @@ class ExcelLibrary:
     def get_sheet_values(self, sheetname, includeEmptyCells=True):
         """
         Returns the values from the sheet name specified.
-
         Arguments:
                 |  Sheet Name (string)                 | The selected sheet that the cell values will be returned from.                                                              |
                 |  Include Empty Cells (default=True)  | The empty cells will be included by default. To deactivate and only return cells with values, pass 'False' in the variable. |
         Example:
-
         | *Keywords*           |  *Parameters*                                      |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Sheet Values     |  TestSheet1                                        |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -238,15 +206,12 @@ class ExcelLibrary:
     def get_workbook_values(self, includeEmptyCells=True):
         """
         Returns the values from each sheet of the current workbook.
-
         Arguments:
                 |  Include Empty Cells (default=True)  | The empty cells will be included by default. To deactivate and only return cells with values, pass 'False' in the variable. |
         Example:
-
         | *Keywords*           |  *Parameters*                                      |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |
         | Get Workbook Values  |                                                    |
-
         """
         sheetData = []
         workbookData = []
@@ -262,16 +227,13 @@ class ExcelLibrary:
     def read_cell_data_by_name(self, sheetname, cell_name):
         """
         Uses the cell name to return the data from that cell.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the cell value will be returned from.  |
                 |  Cell Name (string)   | The selected cell name that the value will be returned from.   |
         Example:
-
         | *Keywords*           |  *Parameters*                                             |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |      |
         | Get Cell Data        |  TestSheet1                                        |  A2  |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -285,17 +247,14 @@ class ExcelLibrary:
     def read_cell_data_by_coordinates(self, sheetname, column, row):
         """
         Uses the column and row to return the data from that cell.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the cell value will be returned from.         |
                 |  Column (int)         | The column integer value that the cell value will be returned from.   |
                 |  Row (int)            | The row integer value that the cell value will be returned from.      |
         Example:
-
         | *Keywords*     |  *Parameters*                                              |
         | Open Excel     |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |   |   |
         | Read Cell      |  TestSheet1                                        | 0 | 0 |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -305,17 +264,14 @@ class ExcelLibrary:
     def read_cell_data_by_header_name(self, sheetname, column_name, row_name):
         """
         Uses the column name and row name value to return the data from that cell.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the cell value will be returned from.         |
                 |  Column Name(string)  | The column name in header that the cell value will be returned from.   |
                 |  Row Name(string)     | The row name in header that the cell value will be returned from.      |
         Example:
-
         | *Keywords*     |  *Parameters*                                              |
         | Open Excel     |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |   |   |
         | Read Cell      |  TestSheet1                                        | Address | John |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -331,17 +287,14 @@ class ExcelLibrary:
     def check_cell_type(self, sheetname, column, row):
         """
         Checks the type of value that is within the cell of the sheet name selected.
-
         Arguments:
                 |  Sheet Name (string)  | The selected sheet that the cell type will be checked from.          |
                 |  Column (int)         | The column integer value that will be used to check the cell type.   |
                 |  Row (int)            | The row integer value that will be used to check the cell type.      |
         Example:
-
         | *Keywords*           |  *Parameters*                                              |
         | Open Excel           |  C:\\Python27\\ExcelRobotTest\\ExcelRobotTest.xls  |   |   |
         | Check Cell Type      |  TestSheet1                                        | 0 | 0 |
-
         """
         my_sheet_index = self.sheetNames.index(sheetname)
         sheet = self.wb.sheet_by_index(my_sheet_index)
@@ -362,4 +315,3 @@ class ExcelLibrary:
             print "The cell value is empty"
         else:
             print error_text_from_code[sheet.cell(row, column).value]
-
